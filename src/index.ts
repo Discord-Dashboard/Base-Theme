3c6c321fd25fa4f50b6223bffddb6cd032de8acb
Marcin Kondrat <breftejk@gmail.com>
2024-07-01 12:49:52 +0200

    Automated commit (from package.json)

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,13 @@ import { Dashboard } from '@discord-dashboard/core';
 import Theme from '@discord-dashboard/typings/dist/Dashboard/Theme';
 import FastifyVite from '@fastify/vite';
 import { resolve, join } from 'node:path';
-import { fileURLToPath } from 'url';
-
-console.log(join(fileURLToPath(import.meta.url), '../../'));
 
 export default class BaseTheme implements Theme {
   name = 'Base';
 
   async Initialize(dashboard: Dashboard) {
     await dashboard.fastify.register(FastifyVite, {
-      root: resolve(join(fileURLToPath(import.meta.url), '../../')),
+      root: resolve(join(__dirname, '../../')),
       renderer: '@fastify/react',
     });
 
