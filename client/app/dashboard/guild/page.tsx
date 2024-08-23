1e1dbe012ceec3a287a6f380335c7588153b1d41
DJKnaeckebrot <dennis.lueke@pm.me>
2024-08-23 15:55:11 +0200

    feat: added navbar to page

--- client/app/dashboard/guild/page.tsx
+++ client/app/dashboard/guild/page.tsx
@@ -2,12 +2,16 @@
 
 import { GuildsListManager } from '@discord-dashboard/react/dist/GuildsListManager';
 import GuildsList from '../../components/GuildsList';
+import Navbar from "../../components/Navbar";
 
 const GuildsPage: React.FC = () => {
     return (
+        <>
+        <Navbar />
         <GuildsListManager>
             <GuildsList />
         </GuildsListManager>
+        </>
     );
 };
 
