82b5a496ce58f3b41db7df37c33bb0e45a95f612
DJKnaeckebrot <dennis.lueke@pm.me>
2024-08-23 15:27:27 +0200

    feat: add tailwindcss

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,11 +1,23 @@
+'use client';
+
+import { Poppins } from 'next/font/google';
+import { SessionProvider } from '../context/SessionContext';
+
+const poppins = Poppins({ subsets: ['latin'], weight: '400' });
+
 export default function RootLayout({
     children,
 }: {
     children: React.ReactNode;
 }) {
     return (
-        <html lang="en">
-            <body>{children}</body>
+        <html lang="en" className="h-full bg-white">
+            <head>
+                <script src="https://cdn.tailwindcss.com"></script>
+            </head>
+            <body className={poppins.className}>
+                <SessionProvider>{children}</SessionProvider>
+            </body>
         </html>
     );
 }
