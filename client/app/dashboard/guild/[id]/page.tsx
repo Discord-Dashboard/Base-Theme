0cf7315da688bc91a6ed84c00cf68603eee5bd13
DJKnaeckebrot <dennis.lueke@pm.me>
2024-08-25 10:31:30 +0200

    fix: issues after merge

--- client/app/dashboard/guild/[id]/page.tsx
+++ client/app/dashboard/guild/[id]/page.tsx
@@ -1,32 +1,13 @@
 'use client';
 
-<<<<<<< Updated upstream
 import Navbar from '../../../components/Navbar';
-import {
-    GuildOptionsManager,
-    useGuildOptionsManager,
-} from '@discord-dashboard/react/dist/GuildOptionsManager';
-import GuildOption from '../../../components/GuildOptions';
+import { GuildOptionsManager } from '@discord-dashboard/react/dist/GuildOptionsManager';
+import GuildOption from '../../../components/GuildOption';
 
-const GuildPage: React.FC = ({ params }: { params: { id: string } }) => {
-    //const guild = useGuildOptionsManager();
-
-    //console.log('Guild: ' + guild);
-    return (
-        <>
-            <GuildOptionsManager>
-=======
-import { GuildsListManager } from '@discord-dashboard/react/dist/GuildsListManager';
-import Navbar from "../../../components/Navbar";
-import {GuildOptionsManager} from "@discord-dashboard/react/dist/GuildOptionsManager";
-import GuildOption from "../../../components/GuildOption";
-
-
-const GuildsPage: React.FC = ({params}: { params: { id: string }}) => {
+const GuildsPage: React.FC = ({ params }: { params: { id: string } }) => {
     return (
         <>
             <GuildOptionsManager guildId={params.id}>
->>>>>>> Stashed changes
                 <Navbar />
                 <GuildOption />
             </GuildOptionsManager>
@@ -34,8 +15,4 @@ const GuildsPage: React.FC = ({params}: { params: { id: string }}) => {
     );
 };
 
-<<<<<<< Updated upstream
-export default GuildPage;
-=======
 export default GuildsPage;
->>>>>>> Stashed changes
