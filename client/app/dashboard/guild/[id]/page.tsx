98ff5999414735a2cfa1e3954cf7a4aaa6a8f979
DJKnaeckebrot <dennis.lueke@pm.me>
2024-08-26 07:44:46 +0200

    feat: add /guild/<id> and /guild/<id>/<category> pages

--- client/app/dashboard/guild/[id]/page.tsx
+++ client/app/dashboard/guild/[id]/page.tsx
@@ -3,14 +3,23 @@
 import Navbar from '../../../components/Navbar';
 import { GuildOptionsManager } from '@discord-dashboard/react/dist/GuildOptionsManager';
 import GuildOption from '../../../components/GuildOption';
+import { UpdateProvider } from '../../../../context/UpdateContext';
+import { GuildCategoriesManager } from '@discord-dashboard/react/dist/GuildCategoriesManager';
+import GuildCateories from '../../../components/GuildCategories';
 
 const GuildsPage: React.FC = ({ params }: { params: { id: string } }) => {
     return (
         <>
-            <GuildOptionsManager guildId={params.id}>
+            {/*<GuildOptionsManager guildId={params.id}>*/}
+            {/*    <UpdateProvider>*/}
+            {/*        <Navbar />*/}
+            {/*        <GuildOption />*/}
+            {/*    </UpdateProvider>*/}
+            {/*</GuildOptionsManager>*/}
+            <GuildCategoriesManager id={params.id}>
                 <Navbar />
-                <GuildOption />
-            </GuildOptionsManager>
+                <GuildCateories />
+            </GuildCategoriesManager>
         </>
     );
 };
